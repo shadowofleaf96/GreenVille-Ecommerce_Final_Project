e832e08b3d18722032aab8a70751f32c97be5ed9
Shadow Of Leaf <simokb46@gmail.com>
2024-01-13 19:53:01 +0100

    return this code

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: '../.env' });
 const database = require('../server/config/database');
 
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on ${process.env.VITE_API_URL}`);
 });
